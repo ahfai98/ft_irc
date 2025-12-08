ec24637ce6aa66e3984534404d47758b39f56d9d
Yap Jien Fei <yapjienfei1998@gmail.com>
2025-12-08 12:23:45 +0800

    Complete PING Command

--- src/ping.cpp
+++ src/ping.cpp
@@ -2,6 +2,32 @@
 
 void Server::PING(const std::string &cmd, int fd)
 {
-    std::cout << "NICK command from " << fd << std::endl;
-	std::cout << cmd << std::endl;
+    Client *cli = getClientByFd(fd);
+    if (!cli)
+        return;
+    std::vector<std::string> tokens = splitCommand(cmd);
+    if (tokens.size() < 2)
+    {
+        sendResponse(fd, "409 :No origin specified\r\n");
+        return;
+    }
+    std::string token = tokens[1];
+    bool trailing = (token.size() > 0 && token[0] == ':');
+    if (!trailing && tokens.size() > 2)
+    {
+        sendResponse(fd, "461 " + cli->getNickname() + " PING :Too many parameters\r\n");
+        return;
+    }
+    for (size_t i = (trailing ? 1 : 0); i < token.size(); ++i)
+    {
+        unsigned char c = token[i];
+        if (c <= 31 || c == 127)
+        {
+            sendResponse(fd, "461 " + cli->getNickname() + " PING :Invalid characters in token\r\n");
+            return;
+        }
+    }
+    if (!trailing)
+        token = ":" + token;
+    sendResponse(fd, "PONG " + token + "\r\n");
 }
