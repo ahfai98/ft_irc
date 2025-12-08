5c2417d389d985a3c981a3e3fdfdf57ca65b5b0c
Yap Jien Fei <yapjienfei1998@gmail.com>
2025-12-08 14:59:59 +0800

    Complete PART command

--- src/part.cpp
+++ src/part.cpp
@@ -2,6 +2,54 @@
 
 void Server::PART(const std::string &cmd, int fd)
 {
-    std::cout << "NICK command from " << fd << std::endl;
-	std::cout << cmd << std::endl;
+    Client *client = getClientByFd(fd);
+    if (!client)
+        return;
+    std::vector<std::string> tokens = splitCommand(cmd);
+    std::string nickname = client->getNickname();
+    if (tokens.size() < 2)
+    {
+        sendResponse(fd, "461 " + nickname + " PART :Not enough parameters\r\n");
+        return;
+    }
+    std::vector<std::string> channels = splitString(tokens[1], ',');
+    std::string partMessage = nickname + " has left the channel";
+    if (tokens.size() > 2)
+    {
+        partMessage = tokens[2];
+        if (!partMessage.empty() && partMessage[0] == ':')
+            partMessage = partMessage.substr(1);
+    }
+    for (size_t i = 0; i < channels.size(); ++i)
+    {
+        std::string chName = channels[i];
+        if (!isValidChannelName(chName))
+        {
+            sendResponse(fd, "403 " + nickname + " " + chName + " :No such channel\r\n");
+            continue;
+        }
+        std::string internalChannelName = chName.substr(1);
+        Channel *ch = getChannel(internalChannelName);
+        if (!ch)
+        {
+            sendResponse(fd, "403 " + nickname + " " + chName + " :No such channel\r\n");
+            continue;
+        }
+        if (!ch->isInChannel(nickname))
+        {
+            sendResponse(fd, "442 " + nickname + " " + chName + " :You're not on that channel\r\n");
+            continue;
+        }
+        std::ostringstream oss;
+        oss << ":" << client->getPrefix() << " PART " << chName << " :" << partMessage << "\r\n";
+        ch->broadcastMessage(oss.str());
+        if (ch->isChannelOperator(nickname))
+        {
+            ch->removeOperatorByFd(fd);
+            if (ch->getChannelTotalClientCount() == 0)
+                removeChannel(chName);
+        }
+        else
+            ch->removeMemberByFd(fd);
+    }
 }
